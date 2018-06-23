507ca80eb437e6a0b42c2786effa3caca79f6ec3
Carl-Philipp Harmant <cp.harmant@gmail.com>
2018-06-22 19:51:59 -0500

    Remove acra

--- app/src/main/java/me/zeeroooo/materialfb/activity/MaterialFacebookApplication.java
+++ app/src/main/java/me/zeeroooo/materialfb/activity/MaterialFacebookApplication.java
@@ -1,18 +1,9 @@
 package me.zeeroooo.materialfb.activity;
 
 import android.app.Application;
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
 
 /**
  * Created by ZeeRooo on 15/04/18
  */
-
-@ReportsCrashes(mailTo = "putYourOwnEmail...")
 public class MaterialFacebookApplication extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        ACRA.init(this);
-    }
 }
